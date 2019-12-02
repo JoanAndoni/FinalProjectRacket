35bce745c82fec8a3ea5fa853649f3bfa4a9a66a
JoanAndoni <mcr_joan@hotmail.com>
2019-12-02 13:36:03 -0600

    Running already

--- textAnalyse.rkt
+++ textAnalyse.rkt
@@ -14,46 +14,52 @@
 	(display "This program analyses an exported chat txt file\n Please enter the name fo the file between quotes\n like this:  \"file.txt\"\n ")
 )
 
-(define (most-frequent-element xs)
-  (define ht (make-hash))
-  (for ([x xs]) (hash-update! ht x add1 0))
-  (for/fold ([max-x #f] [max-count 0]) ([(x c) ht])
-    (if (> c max-count)
-        (values x c)
-        (values max-x max-count)))
+(define (bagify lst)
+    (foldl 
+        ( lambda 
+            (key ht)
+            (hash-update ht key add1 0)
+        )
+        #hash() lst
+    )
 )
 
-(define words2track (file->list "words2track.txt" ))
+(define (appendWordsInList2track lines finalWords)
+    (append (string-split (car lines)) finalWords)
+)
+
+(define words2track (appendWordsInList2track (file->lines "words2track.txt") '()))
 
 (define (contains diccionary word)
     (cond
-        ((null? diccionary) 0)
-        ((equal? (car diccionary) word) 1)
+        ((null? diccionary) #f)
+        ((equal? (car diccionary) word) (list word))
         (else (contains (cdr diccionary) word))
     )
 )
 
-(define (comparisonOfWords words2compare numberOfWords numberOfAppearances)
+(define (comparisonOfWords listRepeated words2compare numberOfWords numberOfAppearances)
     (cond
-        ((null? words2compare) (printResults numberOfAppearances numberOfWords (/ numberOfAppearances numberOfWords)))
-        (else (comparisonOfWords (cdr words2compare) (+ numberOfWords 1) (+ numberOfAppearances (contains words2track (car words2compare)))))
+        ((null? words2compare) (printResults numberOfAppearances numberOfWords (/ numberOfAppearances numberOfWords) listRepeated))
+        ((contains words2track (car words2compare)) (comparisonOfWords (append listRepeated (contains words2track (car words2compare))) (cdr words2compare) (+ numberOfWords 1) (+ numberOfAppearances 1)))
+        ((not (contains words2track (car words2compare))) (comparisonOfWords listRepeated (cdr words2compare) (+ numberOfWords 1) numberOfAppearances))
     )
 )
 
 (define (appendWords lines finalWords)
     (cond
-        ((null? lines) (comparisonOfWords finalWords 0 0))
+        ((null? lines) (comparisonOfWords '() finalWords 0 0))
         (else (appendWords (cdr lines) (append (string-split (car lines)) finalWords)))
     )
 )
 
-(define (printResults numberOfApperances numberOfWords ratio)
+(define (printResults numberOfApperances numberOfWords ratio listRepeated)
   (printf "There were ~a words in the chat ~n" numberOfWords)
   (printf "Your selected words  appeared ~a times ~n" numberOfApperances)
   (printf "That makes ~a% of all the words ~n" (exact->inexact (* ratio 100)))
-  (printf "List of words to search is : ~a ~n" (file->list "words2track.txt"))
+  (printf "List of words to search is : ~a ~n" (appendWordsInList2track (file->lines "words2track.txt") '()))
   (printf "The most frequent words which apppeared is: ")
-  (most-frequent-element (file->list "WhatsAppChat.txt"))
+  (bagify listRepeated)
 )
 
 (welcome)
