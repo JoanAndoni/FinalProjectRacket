33250ce0aade040f359f9d4138421b834884a349
MrGarrowson <enriquevac94@gmail.com>
2019-12-02 11:45:03 -0600

    Saludos

--- textAnalyse.rkt
+++ textAnalyse.rkt
@@ -1,14 +1,28 @@
 #|
     Joan Andoni Gonźalez Rioz
     A00569929
-    Enrique Badillo
-    A0
+    Enrique Vadillo
+    A01018714
 
     Program analyze the exported chats from whats app and facebook in format .txt and
     returns the percentage of used words that you could get tracked.
 |#
 
-(define words2track (list "This" "only" "y"))
+#lang racket
+
+(define (welcome)
+	(display "This program analyses an exported chat txt file\n Please enter the name fo the file between quotes\n like this:  \"file.txt\"\n ")
+)
+
+(define (most-frequent-element xs)
+  (define ht (make-hash))
+  (for ([x xs]) (hash-update! ht x add1 0))
+  (argmax (lambda (x) (hash-ref ht x 0))
+	  (hash->list ht))
+)
+
+(define words2track (file->list "words2track.txt" ))
+
 
 (define (contains diccionary word)
     (cond
@@ -20,7 +34,7 @@
 
 (define (comparisonOfWords words2compare numberOfWords numberOfAppearances)
     (cond
-        ((null? words2compare) (list numberOfAppearances numberOfWords (/ numberOfAppearances numberOfWords)))
+        ((null? words2compare) (printResults numberOfAppearances numberOfWords (/ numberOfAppearances numberOfWords)))
         (else (comparisonOfWords (cdr words2compare) (+ numberOfWords 1) (+ numberOfAppearances (contains words2track (car words2compare)))))
     )
 )
@@ -31,5 +45,12 @@
         (else (appendWords (cdr lines) (append (string-split (car lines)) finalWords)))
     )
 )
-
-(appendWords (file->lines "WhatsAppChat.txt") '())
+(define (printResults numberOfApperances numberOfWords ratio)
+  (printf "There were ~a words in the chat ~n" numberOfWords)
+  (printf "Your selected words  appeared ~a times ~n" numberOfApperances)
+  (printf "That makes ~a% of all the words ~n" (exact->inexact (* ratio 100)))
+  (printf "List of words to search is : ~a ~n" (file->list "words2track.txt"))
+  (printf "The most frequent words ~a ~n" (most-frequent-element (file->list "words2track.txt") ))
+  )
+(welcome)
+(appendWords (file->lines (read)) '())
